8e0444c4634e71ab198f5f85d03d796063310f02
Akshaya2806 <166032953+Akshaya2806@users.noreply.github.com>
2026-03-15 12:41:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    a = float(request.form["a"])
+    b = float(request.form["b"])
+    c = float(request.form["c"])
+    d = float(request.form["d"])
+
+    prediction = model.predict([[a,b,c,d]])
+
+    return render_template("index.html", result=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
